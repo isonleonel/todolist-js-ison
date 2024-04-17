fab09a3eb7d2ec26f42ea0620d77c1ed6d6958fb
isonleonel <m.ison@itecriocuarto.org.ar>
2024-04-17 00:40:52 -0300

    primeros archivos agregados

---
+++ todolist.js
@@ -0,0 +1,9 @@
+let user_tasks = [
+    { task : "Lorem ipsum", completed : false },
+    { task : "dolor sit", completed : false },
+    { task : "amet consectetur", completed : false },
+    { task : "adipisicing elit", completed : false },
+    { task : "Doloribus quam dolor", completed : false },
+    { task : "temporibus esse repellendus", completed : false },
+]
+
