ec763e414703ef03e9b229b196563e17502e3a9f
Leonel Ison <m.ison@itecriocuarto.org.ar>
2024-04-17 01:18:19 -0300

    tareas renderizadas

--- todolist.js
+++ todolist.js
@@ -7,3 +7,20 @@ let user_tasks = [
     { task : "temporibus esse repellendus", completed : false },
 ]
 
+const showTasks = () => {
+  let taskListContainer = document.getElementById("taskList");
+
+  user_tasks.forEach((task, index) => {
+    let card = document.createElement("div");
+    card.className = "card mb-3";
+    card.innerHTML = `
+      <div class="card-body">
+        <h5 class="card-title">${task.task}</h5>
+        <p class="card-text">${task.completed ? 'Completed' : 'Incomplete'}</p>
+      <div>
+    `;
+    taskListContainer.appendChild(card);
+  });
+}
+
+showTasks();
