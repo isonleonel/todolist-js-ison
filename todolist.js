0f513dda6b3eeaf8fce992737084fc9b075e9f4e
Leonel Ison <m.ison@itecriocuarto.org.ar>
2024-04-22 18:02:34 -0300

    tareas completadas

--- todolist.js
+++ todolist.js
@@ -7,42 +7,100 @@ let user_tasks = [
     { task : "temporibus esse repellendus", completed : false },
 ]
 
-const showTasks = () => {
-  let taskListContainer = document.getElementById("taskList");
-  taskListContainer.innerHTML = "";
+// Event listener para el dropdown
+const addDropdownListeners = () => {
+  document.getElementById("allTasks").addEventListener("click", showAllTasks);
+  document.getElementById("completedTasks").addEventListener("click", showCompletedTasks);
+  document.getElementById("incompleteTasks").addEventListener("click", showIncompleteTasks);
+}
 
-  user_tasks.forEach((task, index) => {
-    let card = document.createElement("div");
-    card.className = "card mb-3";
-    card.innerHTML = `
-      <div class="card-body">
-        <h5 class="card-title">${task.task}</h5>
-        <p class="card-text">${task.completed ? 'Completado' : 'Sin completar'}</p>
-      <div>
-    `;
-    taskListContainer.appendChild(card);
+// Event listener para el boton de borrar tarea
+const addCloseButtonListeners = () => {
+  const closeButtons = document.querySelectorAll('.close-button');
+  closeButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      deleteTask(index);
+    });
   });
 }
 
+// Funcion para mostrar todas las tareas
+const showAllTasks = () => {
+  showTasks(user_tasks);
+}
+
+// Funcion para ver solo las tareas completadas
+const showCompletedTasks = () => {
+  const completedTasks = user_tasks.filter(task => task.completed);
+  showTasks(completedTasks);
+}
+
+// Funcion para ver las tareas incompletas
+const showIncompleteTasks = () => {
+  const incompleteTasks = user_tasks.filter(task => !task.completed);
+  showTasks(incompleteTasks);
+}
+
+// Funcion para eliminar una tarea
+const deleteTask = (index) => {
+  user_tasks.splice(index, 1); 
+  showTasks(user_tasks); 
+}
+
+// Renderizar las tareas
+const showTasks = (tasks) => {
+  let taskListContainer = document.getElementById("taskList");
+  taskListContainer.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    let card = document.createElement("div");
+    card.className = "col-md-4";
+    card.innerHTML = `
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 class="card-title">${task.task}</h5>
+        <p class="card-text">
+          <button type="button" class="btn ${
+            task.completed ? 'btn-success' : 'btn-danger'
+          }" onclick="toggleTaskState(${index})">${
+            task.completed ? 'Completada' : 'Sin completar'
+          }</button>
+        </p>
+        <button type="button" class="btn-close close-button" aria-label="Close"></button>
+      </div>
+    </div>
+    `;
+    taskListContainer.appendChild(card);
+  });
+
+  addCloseButtonListeners();
+  addDropdownListeners();
+  
+}
+
+// Cambiar el estado del boton
+const toggleTaskState = (index) => {
+    user_tasks[index].completed = !user_tasks[index].completed;
+    showTasks(user_tasks);
+  }
+
+// Agregar tarea
 document.getElementById("addTaskForm").addEventListener("submit", (event) => {
     event.preventDefault(); 
-    
-    
+
     let taskNameInput = document.getElementById("taskName");
     let taskName = taskNameInput.value.trim();
     
     if (taskName !== "") {
-      
+
       user_tasks.push({ task: taskName, completed: false });
-      
-      
       taskNameInput.value = "";
-      
-      showTasks();
+      showTasks(user_tasks);
+
     } else {
       alert("Por favor, ingrese el nombre de una tarea.");
     }
-  });
+});
 
 
-showTasks();
+showTasks(user_tasks);
